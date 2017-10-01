d130bff26df7667297d1d3febd3aa92c5213e2c5
Hiroki Minami <rafael.hiroki@gmail.com>
2017-10-01 17:17:43 -0300

    Cria tela de cadastro de receitas

--- src/app/content/send-recipe/send-recipe.component.ts
+++ src/app/content/send-recipe/send-recipe.component.ts
@@ -1,14 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  templateUrl: './send-recipe.component.html',
-  styleUrls: ['./send-recipe.component.scss']
+    templateUrl: './send-recipe.component.html',
+    styleUrls: ['./send-recipe.component.scss']
 })
 export class SendRecipeComponent implements OnInit {
+    // TODO Isso vai ser pego de um serviço
+    categories = ['Sobremesa', 'Salada', 'Prato Principal', 'Entrada'];
+    constructor() { }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
