98c9376c14809d453b2c930a1e93728511006354
Hiroki Minami <rafael.hiroki@gmail.com>
2017-10-01 16:41:36 -0300

    Tela de cadastro

--- src/app/content/content.module.ts
+++ src/app/content/content.module.ts
@@ -14,6 +14,7 @@ import { CategoriesComponent } from './categories/categories.component';
 import { LoginComponent } from './login/login.component';
 import { SendRecipeComponent } from './send-recipe/send-recipe.component';
 import { SearchResultsComponent } from './search-results/search-results.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   imports: [
@@ -30,6 +31,7 @@ import { SearchResultsComponent } from './search-results/search-results.componen
     LoginComponent,
     SendRecipeComponent,
     SearchResultsComponent,
+    SignupComponent,
   ],
   exports: [
     ContentComponent,
