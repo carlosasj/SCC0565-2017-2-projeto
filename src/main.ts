a7a54901a13a269a8196e462db3859e0439885cb
Carlos Schneider <carlosaschjr@gmail.com>
2017-10-01 17:17:08 -0300

    Cria página principal

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import * as moment from 'moment';
+import 'moment/locale/pt-br';
+
 if (environment.production) {
   enableProdMode();
 }
