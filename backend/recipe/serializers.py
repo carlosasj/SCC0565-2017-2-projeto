483288de22c8abcdc5536868f391ba4068b286b2
Carlos Schneider <carlosaschjr@gmail.com>
2017-11-19 20:36:39 -0200

    Cria serviço da Home

--- backend/recipe/serializers.py
+++ backend/recipe/serializers.py
@@ -16,6 +16,10 @@ class IngredientSerializer(serializers.ModelSerializer):
 
 
 class RecipeSerializer(serializers.ModelSerializer):
+    """
+    Serializer para criar uma nova receita (o Frontend pode passar só o ID da
+    categoria, e o autor é inserido automaticamente conforme o usuário logado)
+    """
     author = UserSerializer(
         default=serializers.CurrentUserDefault()
     )
@@ -36,5 +40,21 @@ class RecipeSerializer(serializers.ModelSerializer):
 
 
 class RecipeCompleteSerializer(RecipeSerializer):
+    """
+    Serializer para listar receitas ou mostrar detalhes de uma receita (
+    o autor e a categoria se tornam um objeto completo, ao invés de IDs)
+    """
     category = CategorySerializer(read_only=True)
     author = UserSerializer(read_only=True)
+
+
+class CategoryLimitterSerializer(serializers.ModelSerializer):
+    """
+    Serviço usado na Home, lista todas as categorias junto com as 3 receitas
+    mais recentes
+    """
+    recipes = RecipeSerializer(many=True, source='recipes_limit_3')
+
+    class Meta:
+        model = Category
+        fields = ('id', 'name', 'subtitle', 'recipes')
